7435ef5ff4175313d00f0964b4770c8aa01f809e
Vertmo <basile.pesin@gmail.com>
2019-02-06 17:54:50 +0100

    Restructured samples in prevision of next versions and added eval test

--- test.ml
+++ test.ml
@@ -1,9 +1,9 @@
-let samplesAPS0loc = "../../samples/aps0/" and samplesAPS0nb = 21
+let samplesLoc = "../../samples/" and samplesNb = [|21|]
 
-let generateFileNames loc nb =
+let generateFileNames loc version nb =
   List.init nb (fun i -> let si = (string_of_int i) in
-                 let si = (String.make (3 - (String.length si)) '0')^si in
-                 Printf.sprintf "%sprog%s.aps" loc si)
+                 let si = (String.make (2 - (String.length si)) '0')^si in
+                 Printf.sprintf "%sprog%s%s.aps" loc (string_of_int version) si)
 
 let testParse f =
   if Sys.command (Printf.sprintf "./src/aps.exe -parse %s > /dev/null" f) <> 0
@@ -13,12 +13,21 @@ let testType f =
   if Sys.command (Printf.sprintf "./src/aps.exe -type %s > /dev/null " f) <> 0
   then failwith (Printf.sprintf "Typing failed for file %s" f)
 
-let generateAndTest loc nb =
-  let files = generateFileNames loc nb in
+let testEval f res =
+  let ic = Unix.open_process_in (Printf.sprintf "./src/aps.exe -eval %s" f) in
+  try (
+    if (input_line ic) <> res
+    then failwith (Printf.sprintf "Typing failed for file %s" f)
+  ) with _ -> failwith (Printf.sprintf "Typing failed for file %s" f)
+
+let generateAndTest loc version nb =
+  let files = generateFileNames loc version nb in
   List.iter testParse files;
   Printf.printf "Tested parse for %s successfully !\n" loc;
   List.iter testType files;
-  Printf.printf "Tested typing for %s successfully !\n" loc
+  Printf.printf "Tested typing for %s successfully !\n" loc;
+  let res = open_in (Printf.sprintf "%s/res%s.txt" loc (string_of_int version)) in
+  List.iter (fun f -> testEval f (input_line res)) files
 
 let _ =
-  generateAndTest samplesAPS0loc samplesAPS0nb
+  generateAndTest samplesLoc 0 samplesNb.(0)
